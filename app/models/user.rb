7a7385d17956e6f074602d321e0df5164fd6a282
Everesh <everesh@proton.me>
2025-03-03 14:14:05 +0100

    Add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6, maximum: 25 }
+
+  has_many :posts
 end
