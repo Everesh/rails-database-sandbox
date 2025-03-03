77604037ee13dd45b578e619b670db596fbcd89b
Everesh <everesh@proton.me>
2025-03-03 14:07:19 +0100

    Add validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6, maximum: 25 }
 end
