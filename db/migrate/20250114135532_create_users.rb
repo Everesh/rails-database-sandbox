113b9f9bdb97adf314f05849905ab1d692c73a85
Everesh <everesh@proton.me>
2025-03-03 13:47:40 +0100

    Reinitialize project repo

--- db/migrate/20250114135532_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[8.0]
-  def change
-    create_table :users do |t|
-      t.timestamps
-      t.string :username
-    end
-  end
-end
--- db/migrate/20250114144606_add_foreign_key.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKey < ActiveRecord::Migration[8.0]
-  def change
-    add_column :posts, :user_id, :integer
-  end
-end
